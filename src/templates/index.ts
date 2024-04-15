a5cdc94638b4136b9e5d2d9816a07628d0a75105
ilya_rogachev <ilarogacev@ilya-rogachev.local>
2024-04-15 23:13:49 +0500

    export all templates

---
+++ src/templates/index.ts
@@ -0,0 +1,4 @@
+export { FormTemplate } from './form';
+export { AccountLayout } from './account-layout';
+export { PageErrorTemplate } from './page-error';
+export { ChatsLayout } from './chats-layout';
