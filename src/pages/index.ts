05ef4e8119dd67611dfbe5d598ff58e75d846679
ilya_rogachev <ilarogacev@ilya-rogachev.local>
2024-04-15 23:19:22 +0500

    exports all pages

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export { SignIn } from './sign-in';
+export { SignUp } from './sign-up';
+export { NotFound } from './not-found';
+export { ServerError } from './server-error';
+export { Profile } from './profile';
+export { Chats } from './chats';
