8410eff0559989e10240b985c57144239638d055
ilya_rogachev <ilarogacev@ilya-rogachev.local>
2024-04-16 13:30:32 +0500

    add color prop to input component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { Button } from './button';
 export { Input } from './input';
 export { Divider } from './divider';
 export { Navigate } from './navigate';
+export { ProfileLogo } from './profile-logo';
