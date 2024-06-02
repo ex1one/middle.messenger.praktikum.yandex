299dbfc93156f12afd68bc2c7a73df32aff8b2aa
ilya_rogachev <ilarogacev@ilya-rogachev.local>
2024-06-02 21:33:07 +0500

    add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, './src'),
+      '@public': path.resolve(__dirname, './public'),
+    },
+  },
   root: '.',
   build: {
     outDir: 'dist',
