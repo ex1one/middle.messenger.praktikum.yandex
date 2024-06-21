a12fdf3ef11828a95071333f5d0652a5c8d05597
ex1one <vyvf11@gmail.com>
2024-06-21 21:13:28 +0500

    add router to main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,29 +2,8 @@ import Handlebars from 'handlebars';
 import * as Components from './components';
 import * as Templates from './templates';
 import * as Pages from './pages';
-
-interface MenuItem {
-  link: string;
-  text: string;
-}
-
-const menu: MenuItem[] = [
-  { link: 'sign-in', text: 'Вход' },
-  { link: 'sign-up', text: 'Регистрация' },
-  { link: 'profile', text: 'Профиль' },
-  { link: 'chats', text: 'Чаты' },
-  { link: 'not-found', text: '404' },
-  { link: 'server-error', text: '500' },
-];
-
-const pages = {
-  'sign-in': [Pages.SignIn],
-  'sign-up': [Pages.SignUp],
-  'not-found': [Pages.NotFound],
-  'server-error': [Pages.ServerError],
-  'profile': [Pages.Profile],
-  'chats': [Pages.Chats],
-};
+import router from '@src/router';
+import { PATHES } from '@src/consts';
 
 Object.entries(Components).forEach(([name, component]) => {
   if (typeof component === 'string') {
@@ -38,36 +17,10 @@ Object.entries(Templates).forEach(([name, component]) => {
   }
 });
 
-function navigate(page: string) {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  const [source, context] = pages[page];
-  const container = document.getElementById('content')!;
+router
+  .use(PATHES.SignIn, Pages.SignIn)
+  .use(PATHES.SignUp, Pages.SignUp)
+  .use(PATHES.Profile, Pages.Profile)
+  .use(PATHES.Chats, Pages.Chats);
 
-  if (source instanceof Object) {
-    const page = new source(context);
-    container.innerHTML = '';
-    container.append(page.getContent());
-  } else {
-    container.innerHTML = Handlebars.compile(source)(context);
-  }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  const [page, context] = [Components.Navigate, { menu }];
-  const container = document.getElementById('nav')!;
-  container.innerHTML = Handlebars.compile(page)(context);
-});
-
-document.addEventListener('click', (event) => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  const page = event.target?.getAttribute('page');
-
-  if (page) {
-    navigate(page);
-
-    event.preventDefault();
-    event.stopImmediatePropagation();
-  }
-});
+router.start();
